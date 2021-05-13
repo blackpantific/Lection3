2c46b84a3f52aa42998f3687a1ef24335861d9a1
DESKTOP-HAIVOMG\black <blackpantific@gmail.com>
2021-05-13 21:50:26 +0300

    Add matrix multiplication

--- Lection3/Source.cpp
+++ Lection3/Source.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
+#include <omp.h>
 //#include <CL/opencl.h>
 #define CL_TARGET_OPENCL_VERSION 120
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #include <CL/cl.h>
+#include "conio.h"
 #include <vector>
+#include <string>
 
 
 using namespace std;
@@ -15,29 +18,44 @@ void InformationAboutPlatforms();
 cl_device_id InformationAboutDevice(cl_platform_id* platformID, int numberOfDevice);
 void lection3_computing_sum(cl_context context, cl_int status, cl_command_queue queue, cl_kernel kernel);
 void lection4_computing_sum_arrays(cl_context context, cl_int status, cl_command_queue queue, cl_kernel kernel);
-void lection4_multipl_matrix(cl_context context, cl_int status, cl_command_queue queue, cl_kernel kernel);
+void lection4_multipl_matrix(cl_context context, cl_int status, cl_command_queue queue, cl_kernel kernel,
+	float*& matrix1, float*& matrix2, float*& resultMatrix, int* NKM);
+void get_matrixs_from_file(string input_file_path, int NKM[], float*& matrix1, float*& matrix2, float*& resultMatrix);
 
 int main()
 {
 	int numberOfDevice = 0;//by default
-	string pathInputFile = "";
-	string pathOutputFile = "";
+	string pathInputFile = "C:\\Users\\black\\Desktop\\matrix.txt";
+	string pathOutputFile = "C:\\Users\\black\\Desktop\\matrixResult.txt";
 	int numberOfRealization = 0;//by default
+
+	int NKM[3] = { 0,0,0 };
+
+	float* matrix1 = 0;
+	float* matrix2 = 0;
+	float* resultMatrix = 0;
+
+	get_matrixs_from_file(pathInputFile, NKM, matrix1, matrix2, resultMatrix);//получаем данные по матрицам из файла
+
 
 	cl_platform_id platformID;
 	cl_device_id deviceID = InformationAboutDevice(&platformID, numberOfDevice);
+
 	cl_int status;
 	cl_context context;
 	cl_context_properties properties[3] = { CL_CONTEXT_PLATFORM , (cl_context_properties)platformID, 0};
+	size_t deviceNameSize;
+	char* deviceName;
+
+	//display chosen device name
+	status = clGetDeviceInfo(deviceID, CL_DEVICE_NAME, 0, NULL, &deviceNameSize);
+	deviceName = (char*)malloc(deviceNameSize);
+	status = clGetDeviceInfo(deviceID, CL_DEVICE_NAME, deviceNameSize, deviceName, NULL);
+	printf("\nDevice name - %s\n", deviceName);
 
 	context = clCreateContext(properties, 1, &deviceID, NULL, NULL, &status);
-	
-	
-	cl_command_queue queue;
-	queue = clCreateCommandQueue(context, deviceID, CL_QUEUE_PROFILING_ENABLE, &status);
 
-//#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
-
+	//получаем код программы кернела из файла
 	char* buf = NULL;
 	ifstream in("Program.txt", ios::binary);
 	const size_t size = in.seekg(0, ios::end).tellg();
@@ -49,6 +67,10 @@ int main()
 	buf[size] = 0;
 	in.close();
 	const char* buf_p = buf;
+
+
+	cl_command_queue queue;
+	queue = clCreateCommandQueue(context, deviceID, CL_QUEUE_PROFILING_ENABLE, &status);
 	
 	cl_program program;
 	program = clCreateProgramWithSource(context, 1, &buf_p, &size, &status);
@@ -68,20 +90,15 @@ int main()
 	}
 
 	cl_kernel kernel;
-	kernel = clCreateKernel(program, "matrix_multiplication", &status);//ошибка
+	kernel = clCreateKernel(program, "matrix_multiplication", &status);//ошибка обнаруживается тут
 
-	//lection4_computing_sum_arrays(context, status, queue, kernel);
-	 
-	lection4_multipl_matrix(context, status, queue, kernel);
-	
-	//CL_INVALID_PROGRAM
-	//CL_INVALID_VALUE
-
+	lection4_multipl_matrix(context, status, queue, kernel, matrix1, matrix2, resultMatrix, NKM);
 
 	return 0;
 }
 
-void lection4_multipl_matrix(cl_context context, cl_int status, cl_command_queue queue, cl_kernel kernel)
+void lection4_multipl_matrix(cl_context context, cl_int status, cl_command_queue queue, cl_kernel kernel,
+	float*& matrix1, float*& matrix2, float*& resultMatrix, int* NKM)
 {
 
 	/*for (int i = 0; i < 3; i++)
@@ -106,42 +123,55 @@ void lection4_multipl_matrix(cl_context context, cl_int status, cl_command_queue
 		}
 	}*/
 
-	int a[6] = { 1, 2, 3, 4, 5, 6 }; //(3;2)
-	int b[6] = { 1, 2, 3, 4, 5, 6 };//(2;3)
-	int* c = (int*)malloc(sizeof(int) * 9);
+	for (size_t i = 0; i < 9; i++)
+	{
+		printf("\nc[%d] = %f ", i, resultMatrix[i]);
+	}
 
+	//float a[6] = { 1.0, 2, 3, 4, 5, 6 }; //(3;2)
+	//float b[6] = { 1, 2, 3, 4, 5, 6 };//(2;3)
+	//float* c = (float*)malloc(sizeof(float) * 9);
 
-	cl_mem arg_buffer_a = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int) * 6, NULL, &status);
+	auto matrix1Rows = NKM[2];
+	auto matrix1Columns = NKM[1];
+	auto matrix2Rows = NKM[1];
+	auto matrix2Columns = NKM[0];
+
+	auto matrix1ElementsCount = matrix1Rows * matrix1Columns;
+	auto matrix2ElementsCount = matrix2Rows * matrix2Columns;
+	auto resultMatrixCapacity = matrix1Rows * matrix2Columns;
+
+	cl_mem arg_buffer_a = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * matrix1ElementsCount, NULL, &status);
 	//clReleaseMemObject
 
-	status = clEnqueueWriteBuffer(queue, arg_buffer_a, CL_FALSE, 0, sizeof(int) * 6,
-		&a, 0, NULL, NULL);
+	status = clEnqueueWriteBuffer(queue, arg_buffer_a, CL_FALSE, 0, sizeof(float) * matrix1ElementsCount,
+		&matrix1, 0, NULL, NULL);
 
-	cl_mem arg_buffer_b = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int) * 6, NULL, &status);
+	cl_mem arg_buffer_b = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(float) * matrix2ElementsCount, NULL, &status);
 	//clReleaseMemObject
 
-	status = clEnqueueWriteBuffer(queue, arg_buffer_b, CL_FALSE, 0, sizeof(int) * 6,
-		&b, 0, NULL, NULL);
+	status = clEnqueueWriteBuffer(queue, arg_buffer_b, CL_FALSE, 0, sizeof(float) * matrix2ElementsCount,
+		&matrix2, 0, NULL, NULL);
 
-	cl_mem arg_buffer_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(int) * 9, NULL, &status);
+	cl_mem arg_buffer_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * resultMatrixCapacity, NULL, &status);
 	//clReleaseMemObject
 
-	status = clEnqueueWriteBuffer(queue, arg_buffer_c, CL_FALSE, 0, sizeof(int) * 9,
-		&c, 0, NULL, NULL);
+	status = clEnqueueWriteBuffer(queue, arg_buffer_c, CL_FALSE, 0, sizeof(float) * resultMatrixCapacity,
+		&resultMatrix, 0, NULL, NULL);
 
-	int wA = 2;
-	int wB = 3;
+	//int wA = 2;
+	//int wB = 3;
 
 	status = clSetKernelArg(kernel, 0, sizeof(cl_mem), &arg_buffer_a);
 	status |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &arg_buffer_b);
 	status |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &arg_buffer_c);
-	status |= clSetKernelArg(kernel, 3, sizeof(int), &wA);
-	status |= clSetKernelArg(kernel, 4, sizeof(int), &wB);
+	status |= clSetKernelArg(kernel, 3, sizeof(int), &matrix1Columns);
+	status |= clSetKernelArg(kernel, 4, sizeof(int), &matrix2Columns);
 
 	size_t dimentions = 2;
 	size_t global_work_size[2];
-	global_work_size[0] = 3;
-	global_work_size[1] = 3;
+	global_work_size[0] = matrix1Rows;
+	global_work_size[1] = matrix2Columns;
 
 	cl_event ourEvent = 0;
 
@@ -149,7 +179,7 @@ void lection4_multipl_matrix(cl_context context, cl_int status, cl_command_queue
 		NULL, &ourEvent);
 
 	status = clEnqueueReadBuffer(queue, arg_buffer_c, CL_TRUE, 0,
-		sizeof(int) * 9, c, 0, NULL, NULL);//самый последний ReadBuffer должен быть синхронным(CL_TRUE)
+		sizeof(int) * resultMatrixCapacity, resultMatrix, 0, NULL, NULL);//самый последний ReadBuffer должен быть синхронным(CL_TRUE)
 
 	cl_ulong gstart, gend;
 	double gpuTime;
@@ -161,14 +191,155 @@ void lection4_multipl_matrix(cl_context context, cl_int status, cl_command_queue
 
 
 
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < matrix1Rows; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < matrix2Columns; j++)
 		{
-			printf("\nc[%d][%d] = %d ", i, j, c[i * 3 + j]);
+			printf("\nc[%d][%d] = %f ", i, j, resultMatrix[i * matrix1Rows + j]);
 
 		}
 	}
+}
+
+void get_matrixs_from_file(string input_file_path, int NKM[], float*& matrix1, float*& matrix2, float*& resultMatrix) {
+
+	char* bufIterator = NULL;
+	char* buf = NULL;
+
+	ifstream in("C:\\Users\\black\\Desktop\\matrix.txt", ios::binary);
+	int size = in.seekg(0, ios::end).tellg();
+	if (size == -1)
+		throw "File is empty";
+	in.seekg(0);
+	buf = new char[size + 1];
+	in.read(buf, size);
+	buf[size] = 0;
+	bufIterator = buf;
+	in.close();
+	string tempString = "";
+
+	while (true) {
+
+
+		if (*bufIterator == ' ' || *bufIterator == 13) {
+
+
+			if (NKM[0] == 0) {
+				NKM[0] = stoi(tempString);
+				tempString = "";
+			}
+			else
+				if (NKM[1] == 0) {
+					NKM[1] = stoi(tempString);
+					tempString = "";
+				}
+				else
+					if (NKM[2] == 0) {
+						NKM[2] = stoi(tempString);
+						tempString = "";
+					}
+
+			if (*bufIterator == ' ') {
+				bufIterator++;
+			}
+			else {
+				bufIterator++;
+				bufIterator++;
+				break;
+			}
+
+		}
+		else {
+			tempString += *bufIterator;
+			bufIterator++;
+		}
+
+	}
+
+	auto matrix1Rows = NKM[2];
+	auto matrix1Columns = NKM[1];
+	auto matrix2Rows = NKM[1];
+	auto matrix2Columns = NKM[0];
+
+	auto matrix1ElementsCount = matrix1Rows * matrix1Columns;
+	auto matrix2ElementsCount = matrix2Rows * matrix2Columns;
+
+	matrix1 = (float*)calloc(matrix1ElementsCount, sizeof(float));
+	matrix2 = (float*)calloc(matrix2ElementsCount, sizeof(float));
+
+	int i = 0;
+	while (i != matrix1ElementsCount) {
+
+		if ((int)*bufIterator != 32 && (int)*bufIterator != 13 && (int)*bufIterator != 10 && *bufIterator != '\0')
+		{
+			tempString += *bufIterator;
+			bufIterator++;
+
+		}
+		else
+		{
+			if (tempString == "")
+			{
+				throw "Wrong number exception";
+			}
+			matrix1[i] = stod(tempString);
+			i++;
+			bufIterator++;
+			tempString = "";
+		}
+		if ((int)*bufIterator == 10)
+		{
+			bufIterator++;
+		}
+
+	}
+
+
+	for (size_t i = 0; i < matrix1ElementsCount; i++)
+	{
+		cout << endl;
+		printf("matrix1[%i] = %f", i, matrix1[i]);
+	}
+
+	cout << endl;
+
+	i = 0;
+	while (i != matrix2ElementsCount) {
+
+		if ((int)*bufIterator != 32 && (int)*bufIterator != 13 && (int)*bufIterator != 10 && *bufIterator != '\0')
+		{
+			tempString += *bufIterator;
+			bufIterator++;
+
+		}
+		else
+		{
+			if (tempString == "")
+			{
+				throw "Wrong number exception";
+			}
+			matrix2[i] = stod(tempString);
+			i++;
+			bufIterator++;
+			tempString = "";
+		}
+		if ((int)*bufIterator == 10)
+		{
+			bufIterator++;
+		}
+
+	}
+
+
+	for (size_t i = 0; i < matrix2ElementsCount; i++)
+	{
+		cout << endl;
+		printf("matrix2[%i] = %f", i, matrix2[i]);
+	}
+
+	int resultMatrixCapacity = matrix1Rows * matrix2Columns;
+
+	resultMatrix = (float*)calloc(resultMatrixCapacity, sizeof(float));
 }
 
 void lection4_computing_sum_arrays(cl_context context, cl_int status, cl_command_queue queue, cl_kernel kernel)
@@ -177,12 +348,6 @@ void lection4_computing_sum_arrays(cl_context context, cl_int status, cl_command
 	int b[5] = {6, 7, 8, 9, 10};
 	//int c[5] = {0, 0, 0, 0, 0};
 	int* c =(int*) malloc(sizeof(int) * 5);
-
-
-	for (int i = 0; i < 5; i++)
-	{
-		printf("\n%d", c[i]);
-	}
 
 	cl_mem arg_buffer_a = clCreateBuffer(context, CL_MEM_READ_ONLY, sizeof(int)*5, NULL, &status);
 	//clReleaseMemObject
@@ -276,7 +441,7 @@ cl_device_id InformationAboutDevice(cl_platform_id *platformID, int numberOfDevi
 {
 	cl_uint platformCount;
 	int err = clGetPlatformIDs(0, NULL, &platformCount);//gets number of available platforms
-	printf("Number of platforms - %i\n", platformCount);
+	printf("\nNumber of platforms - %i\n", platformCount);
 	cl_platform_id* platforms = (cl_platform_id*)malloc(platformCount);
 	clGetPlatformIDs(platformCount, platforms, NULL);//gets platform ids
 
@@ -284,8 +449,10 @@ cl_device_id InformationAboutDevice(cl_platform_id *platformID, int numberOfDevi
 	cl_uint numberOfDevices;
 	cl_device_id* devices;
 
-	vector<int> devicesGPU;
-
+	vector<cl_device_id> devicesDiscreteGPU;
+	vector<cl_device_id> devicesIntegratedGPU;
+	vector<cl_device_id> devicesCPU;
+	vector<cl_device_id> allDevicesIDs;
 
 	int er = CL_INVALID_PLATFORM;
 
@@ -297,7 +464,6 @@ cl_device_id InformationAboutDevice(cl_platform_id *platformID, int numberOfDevi
 												CL_DEVICE_TYPE_DEFAULT,
 												CL_DEVICE_TYPE_ALL };
 
-
 	char* paramValue;
 	cl_bool res = false;
 	cl_uint  numberOfUnits = 0;
@@ -305,66 +471,67 @@ cl_device_id InformationAboutDevice(cl_platform_id *platformID, int numberOfDevi
 	
 	for(int i = 0; i<platformCount; i++)
 	{
+		//поиск и сортировка GPU-устройств, поддерживающих OpenCL
 		err = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, 0, NULL, &numberOfDevices);
 
-		if (numberOfDevice == 0 || err != 0) {
-			continue;
-		}
+		if (numberOfDevices != 0 || err == 0) {
 
-		devices = (cl_device_id*)malloc(numberOfDevices);
-		err = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, numberOfDevices, devices, NULL);
+			devices = (cl_device_id*)malloc(numberOfDevices);
+			err = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, numberOfDevices, devices, NULL);
 
-		
-		for (size_t j = 0; j < numberOfDevices; j++)
-		{
-			err = clGetDeviceInfo(devices[j], CL_DEVICE_HOST_UNIFIED_MEMORY, 0, NULL, &paramValueRet);
-			//paramValue = (char*)malloc(paramValueRet);
-			err = clGetDeviceInfo(devices[j], CL_DEVICE_HOST_UNIFIED_MEMORY, paramValueRet, &res, NULL);
-			if (res == false)
+			for (size_t j = 0; j < numberOfDevices; j++)//проверка видеокарты дискретная она или интегрированная
 			{
-				//очистка памяти
-				*platformID = platforms[i];
-				return devices[i];
+				err = clGetDeviceInfo(devices[j], CL_DEVICE_HOST_UNIFIED_MEMORY, 0, NULL, &paramValueRet);
+				//paramValue = (char*)malloc(paramValueRet);
+				err = clGetDeviceInfo(devices[j], CL_DEVICE_HOST_UNIFIED_MEMORY, paramValueRet, &res, NULL);
+				if (res == false)
+				{
+					devicesDiscreteGPU.push_back(devices[j]);
+				}
+				else {
+					devicesIntegratedGPU.push_back(devices[j]);
+				}
 			}
 		}
 
-	
+		numberOfDevices = 0;
 
+		//проверка наличия поддержки OpenCL у CPU
+		err = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_CPU, 0, NULL, &numberOfDevices);
 
-		//err = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, 0, NULL, &numberOfDevices);
-		////platforms[0] - обычно у меня это дискретная интел видеокарта, если 1, то интегрированная
-		//devices = (cl_device_id*)malloc(numberOfDevices);
-		//err = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, numberOfDevices, devices, NULL);
+		if (numberOfDevices != 0 || err == 0) {
 
-		//
-		//err = clGetDeviceInfo(devices[i], CL_DEVICE_HOST_UNIFIED_MEMORY, 0, NULL, &paramValueRet);
-		//paramValue = (char*)malloc(paramValueRet);
-		//err = clGetDeviceInfo(devices[i], CL_DEVICE_HOST_UNIFIED_MEMORY, paramValueRet, &res, NULL);//checking is chosen object is nvidia unit
-		//if(res == false)
-		//{
-		//	//очистка памяти
-		//	//platformID = (cl_platform_id*)malloc(sizeof(cl_platform_id*));
-		//	*platformID = platforms[i];
-		//	return devices[i];
-		//}
+			devices = (cl_device_id*)malloc(numberOfDevices);
+			err = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_CPU, numberOfDevices, devices, NULL);
+
+			for (size_t j = 0; j < numberOfDevices; j++)
+			{
+				devicesCPU.push_back(devices[j]);
+			}
+		}
 	}
-	
 
+	allDevicesIDs.insert(allDevicesIDs.end(), devicesDiscreteGPU.begin(), devicesDiscreteGPU.end());
+	allDevicesIDs.insert(allDevicesIDs.end(), devicesIntegratedGPU.begin(), devicesIntegratedGPU.end());
+	allDevicesIDs.insert(allDevicesIDs.end(), devicesCPU.begin(), devicesCPU.end());
 
+	if (numberOfDevice > allDevicesIDs.size()) {
+		auto id = allDevicesIDs[0];
 
-	//CL_DEVICE_MAX_COMPUTE_UNITS  у меня на видеокарте их 3
-	//CL_DEVICE_HOST_UNIFIED_MEMORY
-	//CL_DEVICE_BUILT_IN_KERNELS   не поддерживает
-	
-	
+		clGetDeviceInfo(id, CL_DEVICE_PLATFORM, 0, NULL, &paramValueRet);
+		clGetDeviceInfo(id, CL_DEVICE_PLATFORM, paramValueRet, platformID, NULL);
 
-	/*error = clGetDeviceInfo(devices[0], CL_DEVICE_BUILT_IN_KERNELS, 0, NULL, &paramValueRet);
-	paramValue = (char*)malloc(paramValueRet);
-	error = clGetDeviceInfo(devices[0], CL_DEVICE_BUILT_IN_KERNELS, paramValueRet, paramValue, NULL);*/
+		return id;
+	}
+	else {
 
-	//printf("%s", paramValue);
-	//printf("%s", res ? "true" : "false");
-	return NULL;
+		auto id = allDevicesIDs[numberOfDevice];
+
+		clGetDeviceInfo(id, CL_DEVICE_PLATFORM, 0, NULL, &paramValueRet);
+		clGetDeviceInfo(id, CL_DEVICE_PLATFORM, paramValueRet, platformID, NULL);
+
+		return id;
+	}
 }
 
 void InformationAboutPlatforms()
